552d778e6cd874dbb675f9d2a2fcaa28953b82ee
Sagar Biradar <sagarbiradar475@gmail.com>
2025-04-30 01:08:32 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,11 +1,2 @@
 print('Hello')
 print('My name is sagar')
-
-def add_numbers(num1, num2):
-    return num1 + num2
-
-number1 = 5
-number2 = 10
-
-result = add_numbers(number1, number2)
-print("The sum is:", result)
